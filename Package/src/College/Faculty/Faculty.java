dc412aafa464dddfa4f1bfa3ea14bdd235486e9c
shivamkuntal56-prog <shivamkuntal56@gmail.com>
2026-03-16 22:52:47 +0530

    [shivam] add my file

---
+++ Package/src/College/Faculty/Faculty.java
@@ -0,0 +1,8 @@
+package College.Faculty;
+
+public class Faculty {
+    public void displayFaculty() {
+        System.out.println("Faculty Name: Tanmay");
+        System.out.println("Subject: Java Programming");
+    }
+}

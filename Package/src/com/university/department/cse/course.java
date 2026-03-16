ad6475a48cd7b5784739fbf0886c2d67a2b2fac1
shivamkuntal56-prog <shivamkuntal56@gmail.com>
2026-03-16 22:50:04 +0530

    [shivam] add my file

---
+++ Package/src/com/university/department/cse/course.java
@@ -0,0 +1,10 @@
+package com.university.department.cse;
+
+public class course {
+    public void showCourse() {
+        System.out.println("Course Name: OOPs");
+        System.out.println("Department: CSE");
+        System.out.println("University: GLA University");
+    }
+}
+
